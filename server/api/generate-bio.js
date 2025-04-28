5c005793bd1be4c98ad9094c05df5b6edc9a9bb0
Adarshdev <adarshdev9349@gmail.com>
2025-04-28 19:51:57 +0530

    api update

--- server/api/generate-bio.js
+++ server/api/generate-bio.js
@@ -1,12 +1,6 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-  // Always set CORS headers FIRST
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  // Handle OPTIONS request immediately
   if (req.method === "OPTIONS") {
     res.status(200).end();
     return;
