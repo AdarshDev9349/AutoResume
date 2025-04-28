0261662e9b872da3030471b2cfac0ed1afbaeec6
Adarshdev <adarshdev9349@gmail.com>
2025-04-28 20:37:41 +0530

    api update

--- server/api/generate-bio.js
+++ server/api/generate-bio.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
 
