6329e6d06c79aac0f392cc11ab8313fa2cbe389a
Adarshdev <adarshdev9349@gmail.com>
2025-04-28 20:26:58 +0530

    api update

--- server/api/generate-bio.js
+++ server/api/generate-bio.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   // Set CORS headers for every request
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
